46a33cec70087d0ba861d0aefd10f3f85bb88de1
arima0714 <32933855+arima0714@users.noreply.github.com>
2022-04-13 16:10:39 +0900

    各カラムの説明を詳しいものにした

--- flyway/sql/V1.0.0__init.sql
+++ flyway/sql/V1.0.0__init.sql
@@ -3,8 +3,8 @@
 CREATE TABLE users
 (
     user_id  int unsigned NOT NULL AUTO_INCREMENT COMMENT 'user_id',
-    email    varchar(254) NOT NULL COMMENT 'email',
-    password varchar(100) NOT NULL COMMENT 'password',
+    email    varchar(254) NOT NULL COMMENT 'ログイン用email',
+    password varchar(100) NOT NULL COMMENT 'ログイン用password',
     PRIMARY KEY (user_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
@@ -15,9 +15,9 @@ CREATE TABLE users
 CREATE TABLE posts
 (
     post_id int unsigned NOT NULL AUTO_INCREMENT COMMENT 'post_id',
-    user_id int unsigned NOT NULL COMMENT 'user_id',
-    body    text NOT NULL COMMENT 'body',
-    date    datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'date',
+    user_id int unsigned NOT NULL COMMENT '投稿者user_id',
+    body    text NOT NULL COMMENT '本文',
+    date    datetime DEFAULT CURRENT_TIMESTAMP COMMENT '投稿日付',
     PRIMARY KEY (post_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
@@ -27,9 +27,9 @@ CREATE TABLE posts
 -- イイネトランザクション
 CREATE TABLE likes
 (
-    post_id int unsigned NOT NULL COMMENT 'post_id',
-    user_id int unsigned NOT NULL COMMENT 'user_id',
-    date    datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'date',
+    post_id int unsigned NOT NULL COMMENT 'イイネ先post_id',
+    user_id int unsigned NOT NULL COMMENT 'イイネをしたユーザのuser_id',
+    date    datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'イイネした日付時刻',
     PRIMARY KEY (post_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
