5e1a2087b81aa1cd11c80ad84366edf9ebb19108
arima0714 <32933855+arima0714@users.noreply.github.com>
2022-04-13 14:05:14 +0900

    postsテーブル初期化sqlを追加

--- flyway/sql/V1.0.0__init.sql
+++ flyway/sql/V1.0.0__init.sql
@@ -2,11 +2,25 @@
 -- ユーザマスタ
 CREATE TABLE users
 (
-    user_id  int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    user_id  int unsigned NOT NULL AUTO_INCREMENT COMMENT 'user_id',
     email    string NOT NULL COMMENT 'email',
     password string NOT NULL COMMENT 'password',
     PRIMARY KEY (user_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci comment ='ユーザマスタ'
+;
+
+
+-- 投稿マスタ
+CREATE TABLE posts
+(
+    post_id int unsigned NOT NULL AUTO_INCREMENT COMMENT 'post_id',
+    user_id int unsigned NOT NULL COMMENT 'user_id',
+    body    string NOT NULL COMMENT 'body',
+    date    datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'date',
+    PRIMARY KEY (post_id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci comment ='投稿マスタ'
 ;
