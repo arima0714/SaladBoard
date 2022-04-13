cd91d64e786d2c3f26894bb66814b5d03d99e8e5
arima0714 <32933855+arima0714@users.noreply.github.com>
2022-04-13 17:10:10 +0900

    posts, likes に外部キー制約をつけた

--- flyway/sql/V1.0.0__init.sql
+++ flyway/sql/V1.0.0__init.sql
@@ -20,6 +20,7 @@ CREATE TABLE posts
     user_id int unsigned NOT NULL COMMENT '投稿者user_id',
     body    text         NOT NULL COMMENT '本文',
     date    datetime DEFAULT CURRENT_TIMESTAMP COMMENT '投稿日付',
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
     PRIMARY KEY (post_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
@@ -33,6 +34,8 @@ CREATE TABLE likes
     post_id int unsigned NOT NULL COMMENT 'イイネ先post_id',
     user_id int unsigned NOT NULL COMMENT 'イイネをしたユーザのuser_id',
     date    datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'イイネした日付時刻',
+    FOREIGN KEY (post_id) REFERENCES posts (post_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
     PRIMARY KEY (post_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
