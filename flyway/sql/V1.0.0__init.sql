4cc518aa0650c2c2abe11251c165dcdf356e3edc
arima0714 <32933855+arima0714@users.noreply.github.com>
2022-04-14 12:39:19 +0900

    カラム名dateをcreated_atに合わせて、posted_at, liked_atに変更

--- flyway/sql/V1.0.0__init.sql
+++ flyway/sql/V1.0.0__init.sql
@@ -16,10 +16,10 @@ CREATE TABLE users
 -- 投稿トランザクション
 CREATE TABLE posts
 (
-    post_id int unsigned NOT NULL AUTO_INCREMENT COMMENT 'post_id',
-    user_id int unsigned NOT NULL COMMENT '投稿者user_id',
-    body    text         NOT NULL COMMENT '本文',
-    date    datetime DEFAULT CURRENT_TIMESTAMP COMMENT '投稿日時',
+    post_id   int unsigned NOT NULL AUTO_INCREMENT COMMENT 'post_id',
+    user_id   int unsigned NOT NULL COMMENT '投稿者user_id',
+    body      text         NOT NULL COMMENT '本文',
+    posted_at datetime DEFAULT CURRENT_TIMESTAMP COMMENT '投稿日時',
     FOREIGN KEY (user_id) REFERENCES users (user_id),
     PRIMARY KEY (post_id)
 ) ENGINE = InnoDB
@@ -31,9 +31,9 @@ CREATE TABLE posts
 -- イイネトランザクション
 CREATE TABLE likes
 (
-    post_id int unsigned NOT NULL COMMENT 'イイネ先post_id',
-    user_id int unsigned NOT NULL COMMENT 'イイネをしたユーザのuser_id',
-    date    datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'イイネした日付時刻',
+    post_id  int unsigned NOT NULL COMMENT 'イイネ先post_id',
+    user_id  int unsigned NOT NULL COMMENT 'イイネをしたユーザのuser_id',
+    liked_at datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'イイネした日付時刻',
     FOREIGN KEY (post_id) REFERENCES posts (post_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id),
     PRIMARY KEY (post_id, user_id)
