c45dc04f2a028d259f4538e5ac0de89e8933066d
arima0714 <32933855+arima0714@users.noreply.github.com>
2022-04-20 11:40:47 +0900

    プロジェクトの設定を追加
    依存関係を追加
    DB接続設定を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("mysql:mysql-connector-java")
 }
 
 tasks.withType<KotlinCompile> {
