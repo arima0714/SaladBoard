ecd4cb70667a232c457b0e8b74d2d70cda11cea3
arima0714 <32933855+arima0714@users.noreply.github.com>
2022-04-20 17:08:22 +0900

    ダミー投稿をflywayを用いてDBに入れる処理を追加

---
+++ flyway/sql/V1.0.2__insert_posts.sql
@@ -0,0 +1,39 @@
+use db;
+INSERT INTO posts (user_id, body)
+VALUES (1, 'test body by 1');
+
+use db;
+INSERT INTO posts (user_id, body)
+VALUES (2, 'test body by 2');
+
+use db;
+INSERT INTO posts (user_id, body)
+VALUES (3, 'test body by 3');
+
+use db;
+INSERT INTO posts (user_id, body)
+VALUES (1, 'user 1 による投稿');
+
+use db;
+INSERT INTO posts (user_id, body)
+VALUES (2, 'user 2 による投稿');
+
+use db;
+INSERT INTO posts (user_id, body)
+VALUES (3, 'user 3 による投稿');
+
+use db;
+INSERT INTO posts (user_id, body)
+VALUES (4, 'test body by 4');
+
+use db;
+INSERT INTO posts (user_id, body)
+VALUES (5, 'test body by 5');
+
+use db;
+INSERT INTO posts (user_id, body)
+VALUES (4, 'user 4 による投稿');
+
+use db;
+INSERT INTO posts (user_id, body)
+VALUES (5, 'user 5 による投稿');
